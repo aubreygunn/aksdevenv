dff11ed0134669e42c93a891162acf339c057e79
Kerwin Stanley <kerwin.stanley@vmlyr.com>
2023-08-23 14:19:18 -0500

    Added key vault policy

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -1,4 +1,27 @@
 # This policy must be kept for a proper run of the "destroy" process
+
+data "azurerm_client_config" "current" {}
+data "azuread_client_config" "current" {}
+
+# Create the Azure Key Vault
+resource "azurerm_key_vault" "key_vault" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+
+  tenant_id = data.azuread_client_config.current.tenant_id
+  sku_name  = var.sku_name
+
+  network_acls {
+    default_action = "Allow"
+    bypass         = "AzureServices"
+  }
+}
+
 resource "azurerm_key_vault_access_policy" "default_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = data.azuread_client_config.current.tenant_id
